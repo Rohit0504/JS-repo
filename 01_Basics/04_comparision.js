16afaab28474cd8ef4b5b925c353ca3cc30979da
Rohit Kumar Singh <95770362+Rohit0504@users.noreply.github.com>
2024-02-22 18:12:27 +0000

    compare

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);                 // avoid such comparision , it can cause confusion.
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);  // strictly check the data value and also data type.
+// op = false (as one is string and other one is number).
